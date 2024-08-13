594631606518c1998fb27954bdfcc7d05fa607fb
Jake Rieger <contact.jakerieger@gmail.com>
2024-08-13 03:57:25 -0400

    wip ps shader for fxaa

--- Game/Shaders/FXAA_PS.hlsl
+++ Game/Shaders/FXAA_PS.hlsl
@@ -3,6 +3,14 @@
 Texture2D<float4> Texture : register(t0);
 sampler Sampler : register(s0);
 
+// TODO: Pass these in instead of hardcoding
+const uint ScreenWidth = 1920;
+const uint ScreenHeight = 1080;
+
+float rgb2luma(float3 rgb) {
+    return sqrt(dot(rgb, float3(0.299, 0.587, 0.114)));
+}
+
 float4 main(VSInputTx pin) : SV_Target0 {
     float4 color = Texture.Sample(Sampler, pin.TexCoord);
     return color;
